bfb645a900834a8f7bffbe45d73fc94d1984d5f3
Yordan Hristov <yshristov@gmail.com>
2018-11-23 19:36:49 +0000

    allow 3d inspection of the processed kinect data

---
+++ kinect_processor/src/inspect_cloud.py
@@ -0,0 +1,43 @@
+'''
+==============
+3D scatterplot
+==============
+
+Demonstration of a basic scatterplot in 3D.
+'''
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.load("rosbag_dump/rosbag_dump.npz")
+print(data.files)
+
+clouds = data['arr_0']
+print(len(clouds))
+for cloud in clouds:
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	for i in range(len(cloud[0])):
+		points = cloud[0][i]
+		color = cloud[1][i]
+
+		xs = points[:,0][::3]
+		ys = points[:,1][::3]
+		zs = points[:,2][::3]
+
+		xs_filtered = list(filter(lambda point : (point != [0,0,0]).all(), xs))
+		ys_filtered = list(filter(lambda point : (point != [0,0,0]).all(), ys))
+		zs_filtered = list(filter(lambda point : (point != [0,0,0]).all(), zs))
+
+		# ax.scatter(xs, ys, zs, c=key)
+		ax.scatter(xs_filtered, ys_filtered, zs_filtered, c=color)
+
+	ax.set_xlabel('Z0', fontsize='20', fontweight="bold")
+	# ax.set_xlim(0, 1)
+	ax.set_ylabel('Z1', fontsize='20', fontweight="bold")
+	# ax.set_ylim(0, 1)
+	ax.set_zlabel('Z2', fontsize='20', fontweight="bold")
+	# ax.set_zlim(0, 1)
+
+	plt.show()
