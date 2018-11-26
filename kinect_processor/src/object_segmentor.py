b80a828da3ccd94497401b528171cbcbd2273151
Yordan Hristov <yshristov@gmail.com>
2018-11-26 21:06:38 +0000

    add object groups and corresponding hues for better control over the segmentation

--- kinect_processor/src/object_segmentor.py
+++ kinect_processor/src/object_segmentor.py
@@ -29,13 +29,13 @@ class Object_Segmentor(object):
         self.bounds = {'x':[0.1, 1.3], 'y':[-0.5, 0.5], 'z':[0.0, 1.0]}
 
         # hue ranges - [0,180] - used when classifiying patches wrt color labels
-        self.hues = {}
-        self.hues['blue'] = [90, 130]
-        self.hues['yellow'] = [10, 30]
-        self.hues['green'] = [30, 65]
-        self.hues['red'] = [170, 180]
-        self.hues['purple'] = [130, 170]
-        self.hues['orange'] = [0,10, 175, 180]
+        self.hues = [{},{}]
+        self.hues[1]['blue'] = [90, 130]
+        self.hues[0]['yellow'] = [10, 30]
+        self.hues[0]['green'] = [30, 65]
+        self.hues[0]['red'] = [170, 180]
+        self.hues[0]['purple'] = [130, 170]
+        self.hues[1]['orange'] = [0,10, 175, 180]
 
 
     def process_data(self):
@@ -74,18 +74,26 @@ class Object_Segmentor(object):
             backgrounds = []
             masks = []
             
+            # remove the carpet
             # take patches for bg from the 4 courners
-            backgrounds.append(np.tile(image[0 : bg_patch_size, \
-                                       0 : bg_patch_size, :], \
+            top_left = image[0 : bg_patch_size, 0 : bg_patch_size, :]
+            top_left[(top_left > 150).all(axis=2)] = 0
+            backgrounds.append(np.tile(top_left, \
                                        number_of_tiles))
-            backgrounds.append(np.tile(image[image_size[0] - bg_patch_size : image_size[0], \
-                                       0 : bg_patch_size, :], \
+
+            bottom_left = image[image_size[0] - bg_patch_size : image_size[0], 0 : bg_patch_size, :]
+            bottom_left[(bottom_left > 150).all(axis=2)] = 0
+            backgrounds.append(np.tile(bottom_left, \
                                        number_of_tiles))
-            backgrounds.append(np.tile(image[0 : bg_patch_size, \
-                                       image_size[1] - bg_patch_size : image_size[1], :], \
+
+            top_right = image[0 : bg_patch_size, image_size[1] - bg_patch_size : image_size[1], :]
+            top_right[(top_right > 150).all(axis=2)] = 0
+            backgrounds.append(np.tile(top_right, \
                                        number_of_tiles))
-            backgrounds.append(np.tile(image[image_size[0] - bg_patch_size : image_size[0], \
-                                       image_size[1] - bg_patch_size : image_size[1], :], \
+
+            bottom_right = image[image_size[0] - bg_patch_size : image_size[0], image_size[1] - bg_patch_size : image_size[1], :]
+            bottom_right[(bottom_right > 150).all(axis=2)] = 0
+            backgrounds.append(np.tile(bottom_right, \
                                        number_of_tiles))
 
             # for each background sample generate a binary mask; after that combine them all
@@ -106,18 +114,18 @@ class Object_Segmentor(object):
             mask_extended = np.tile(mask[:,:,np.newaxis], (1,1,3))
             bgr = bgr * mask_extended
 
-
-
             # filter the white parts of the pointcloud
-            mask = np.logical_and(np.any(bgr < 150, axis=2), np.any(bgr > 0, axis=2)).astype(np.uint8)
+            mask = np.logical_and(np.any(bgr < 120, axis=2), np.any(bgr > 0, axis=2)).astype(np.uint8)
             # cv2.imshow("Pure Mask", mask * 255)
 
             kernel = np.ones((5, 5), np.uint8)
             mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
             mask = cv2.dilate(mask, kernel, iterations=1)
 
+            # cv2.imshow("bg_sub_mask", mask_extended * 255)
             # cv2.imshow("Smoothed", mask * 255)
-            # cv2.imshow("bgr", bgr * np.tile(mask[:,:,np.newaxis], (1,1,3)))
+            # cv2.imshow("bgr", bgr)
+            # cv2.imshow("bgr_masked", bgr * np.tile(mask[:,:,np.newaxis], (1,1,3)))
             # cv2.waitKey(0)
 
             mask_copy = copy.deepcopy(mask)
@@ -128,7 +136,7 @@ class Object_Segmentor(object):
             # extract the color and spatial information for each object
             new_mask = np.zeros((height, width))
             new_entry = {}
-            for cnt in contours[:self.args['no_objects']]:
+            for cnt_index, cnt in enumerate(contours[:self.args['no_object_groups']]):
                 M = cv2.moments(cnt)
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
@@ -142,46 +150,45 @@ class Object_Segmentor(object):
                 bbox = list(map(lambda (x, y) : y if x > y else x, zip(bbox, maxes)))
 
                 xyz_crop = xyz[bbox[0] : bbox[1], bbox[2] : bbox[3], :]
-                # xyz_crop_trans = transform_xyz(xyz_crop, target_frame="base_link", height=crop_size * 2, width=crop_size * 2)
-                xyz_crop_trans = xyz_crop
-                xyz_crop_trans_norm = self.normalise_xyz(xyz_crop_trans, bounds=self.bounds)
+                xyz_crop_norm = self.normalise_xyz(xyz_crop, bounds=self.bounds)
                 bgr_crop = bgr[bbox[0] : bbox[1], bbox[2] : bbox[3], :]
 
                 result = {}
 
                 # filter out the white table artefacts (+ object shadows)
-                xyz_crop_trans_norm[(bgr_crop > 120).all(axis=2)] = 0
+                xyz_crop_norm[(bgr_crop > 120).all(axis=2)] = 0
                 bgr_crop[(bgr_crop > 120).all(axis=2)] = 0
+
                 hsv_crop = cv2.cvtColor(bgr_crop, cv2.COLOR_BGR2HSV)
 
-                # cv2.imshow("crop", bgr_crop)
-                # cv2.waitKey(0)
-                # continue
-
-                for hue in self.hues:
-                    mask = (np.logical_and(hsv_crop[...,0] > self.hues[hue][0], hsv_crop[...,0] <= self.hues[hue][1])).astype(np.uint8)
+                for hue, hue_values in self.hues[cnt_index].items():
+                    mask = (np.logical_and(hsv_crop[...,0] > hue_values[0], hsv_crop[...,0] <= hue_values[1])).astype(np.uint8)
                     
-                    # print(hue)
-                    # print(len(mask[mask == 1]))
+                    # smooth out the mask
+                    kernel = np.ones((3, 3), np.uint8)
+                    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-                    # ignore any small patches
-                    if len(mask[mask == 1]) < 500:
-                        continue
+
+                    cv2.imshow("init mask", mask * 255)
 
                     if hue == "orange":
                         # revise the mask to include the low range of red-like colors
-                        mask = (np.logical_or(np.logical_and(hsv_crop[...,0] > self.hues[hue][0], hsv_crop[...,0] <= self.hues[hue][1]), np.logical_and(hsv_crop[...,0] > self.hues[hue][2], hsv_crop[...,0] <= self.hues[hue][3]))).astype(np.uint8)
+                        mask = (np.logical_or(np.logical_and(hsv_crop[...,0] > hue_values[0], hsv_crop[...,0] <= hue_values[1]), np.logical_and(hsv_crop[...,0] > hue_values[2], hsv_crop[...,0] <= hue_values[3]))).astype(np.uint8)
                         
                         kernel = np.ones((5, 5), np.uint8)
                         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
                         mask = self.fill_holes_get_max_cnt(mask)
-                    else:
-                        kernel = np.ones((5, 5), np.uint8)
-                        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-                        # kernel = np.ones((2, 2), np.uint8)
-                        # mask = cv2.dilate(mask, kernel, iterations=1)
 
-                    xyz_object = xyz_crop_trans_norm[mask == 1]
+                    # mask = self.fill_holes_get_max_cnt(mask)
+
+                    # ignore any small patches
+                    if np.sum(mask) < 500:
+                        continue
+
+                    xyz_object = xyz_crop_norm[mask == 1]
+                    
+                    # print(hue)
+                    # print(np.sum(mask))
 
                     # hsv_copy = hsv_crop.copy()
                     # hsv_copy[mask == 0] = 0
@@ -193,10 +200,13 @@ class Object_Segmentor(object):
                     "The data can not be normalised in the range [0,1] - Potentially bad bounds"
 
                     new_entry[hue] = xyz_object
-            
+
+            assert len(new_entry.items()) == self.args['no_objects'], \
+            "Some objects were not extracted for cloud # {0}! {1}".format(self.counter, new_entry.keys())
+                   
             self.output.append(new_entry)
 
-    def fill_holes_get_max_cnt(self, mask):
+    def fill_holes_get_max_cnt(self, mask, fill=-1):
         """Given a binary mask, fills in any holes in the contours, selects the contour with max area
         and returns a mask with only it drawn + its bounding box
         """
@@ -204,8 +214,11 @@ class Object_Segmentor(object):
         cnts, hier = cv2.findContours(mask.astype(np.uint8),cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
         areas = [cv2.contourArea(cnt) for cnt in cnts]
 
+        # for (cnt, area) in sorted(zip(cnts, areas), key=lambda x: x[1], reverse=True):
+        #     cv2.drawContours(canvas,[cnt],0,1,-1)
+
         (cnt, area) = sorted(zip(cnts, areas), key=lambda x: x[1], reverse=True)[0]
-        cv2.drawContours(canvas,[cnt],0,1,-1)
+        cv2.drawContours(canvas,[cnt],0,1,fill)
 
         return canvas.astype(np.uint8)
 
