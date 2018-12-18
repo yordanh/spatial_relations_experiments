c5f158a8c7079c04bc7e69cc73b6b5152084b4cc
Yordan Hristov <yshristov@gmail.com>
2018-12-18 11:50:46 +0000

    update segmented clouds inspection

--- kinect_processor/src/inspect_segmented_clouds.py
+++ kinect_processor/src/inspect_segmented_clouds.py
@@ -22,23 +22,25 @@ parser.add_argument('--every_nth', default=5, type=int,
 args = parser.parse_args()
 
 data = np.load('scenes/' + args.scene + '/segmented_objects.npz')
+cmap = plt.cm.get_cmap('cool')
 
 clouds = data['arr_0']
 print(len(clouds))
 for i, cloud in enumerate(clouds[::args.every_nth]):
 
-	print("{0}/{1}".format(args.every_nth * i, len(clouds)))
+	print("{0}/{1}, {2} objects".format(args.every_nth * i, len(clouds), len(cloud)))
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
-	for color in cloud:
-		points = cloud[color].reshape(200*200,3)
+	for j, label in enumerate(cloud.keys()):
+		points = cloud[label].reshape(200*200,3)
 		filtered_points = np.array(list(filter(lambda row : filter(lambda point : (point != [0,0,0]).all(), row), points)))
 
 		xs = filtered_points[...,0][::3]
 		ys = filtered_points[...,1][::3]
 		zs = filtered_points[...,2][::3]
 
-		ax.scatter(xs, ys, zs, c=color.split('_')[0])
+		# ax.scatter(xs, ys, zs, c=label.split('_')[0])
+		ax.scatter(xs, ys, zs, c=cmap(j / float(len(cloud.keys()))))
 
 	ax.set_xlabel('X', fontsize='20', fontweight="bold")
 	# ax.set_xlim(0, 1)
