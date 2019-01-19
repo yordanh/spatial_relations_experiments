88d25cb80d99431e060d93c9ad605eea64e299d3
Yordan Hristov <yshristov@gmail.com>
2019-01-19 18:28:01 +0000

    revisions of the model + parameter adding

--- learning_experiments/src/net_200x200.py
+++ learning_experiments/src/net_200x200.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-title           :net.py
+title           :net_200x200.py
 description     :Contains definitions of all the models used in the experiments.
 author          :Yordan Hristov <yordan.hristov@ed.ac.uk
 date            :11/2018
@@ -17,18 +17,20 @@ import chainer.links as L
 from chainer.backends.cuda import get_device_from_array
 import cupy
 
+import numpy as np
+
 
 class Operator(chainer.Chain):
     """Fully-connected feedforwards operator"""
 
-    def __init__(self, n_latent=None):
+    def __init__(self, n_latent=None, embed_size=None):
         super(Operator, self).__init__()
         with self.init_scope():
 
             self.n_latent = n_latent
             
             self.operator_0 = L.Linear(2 * self.n_latent, self.n_latent)
-            self.operator_1 = L.Linear(self.n_latent, 1)
+            self.operator_1 = L.Linear(self.n_latent, 2 * embed_size)
 
 
     def __call__(self, z_concat):
@@ -38,7 +40,8 @@ class Operator(chainer.Chain):
 
     def encode(self, z_concat):
 
-        z_tmp = F.relu(self.operator_0(z_concat))
+        # z_tmp = F.leaky_relu(self.operator_0(z_concat))
+        z_tmp = self.operator_0(z_concat)
         z = self.operator_1(z_tmp)
         return z
 
@@ -56,6 +59,7 @@ class Conv_Siam_VAE(chainer.Chain):
             self.gamma= gamma
             self.n_latent = n_latent
             self.groups_len = [len(groups[key]) for key in sorted(groups.keys())]
+            self.group_n = len(self.groups_len)
             self.classifiers = chainer.ChainList()
             self.operators = chainer.ChainList()
 
@@ -103,13 +107,14 @@ class Conv_Siam_VAE(chainer.Chain):
             # of each branch
             for i in range(len(self.groups_len)):
                 self.classifiers.add_link(L.Linear(1, self.groups_len[i]))
-                self.operators.add_link(Operator(self.n_latent))
+                self.operators.add_link(Operator(n_latent=self.n_latent, embed_size=self.group_n))
 
 
             ########################
             # decoder for branch 0 #
             ########################
-            self.decoder_dense_b0_0 = L.Linear(self.n_latent, 64)
+            self.decoder_dense_b0_0 = L.Linear(self.n_latent + len(self.groups_len), 64)
+            # self.decoder_dense_b0_0 = L.Linear(self.n_latent, 64)
             self.decoder_dense_b0_1 = L.Linear(64, 512)
             # reshape from (1, 512) to (8, 8, 8)
             self.decoder_conv_b0_0 = L.Convolution2D(8, 8, ksize=3, pad=1) # (8, 8)
@@ -127,7 +132,8 @@ class Conv_Siam_VAE(chainer.Chain):
             ########################
             # decoder for branch 1 #
             ########################
-            self.decoder_dense_b1_0 = L.Linear(self.n_latent, 64)
+            self.decoder_dense_b1_0 = L.Linear(self.n_latent + len(self.groups_len), 64)
+            # self.decoder_dense_b1_0 = L.Linear(self.n_latent, 64)
             self.decoder_dense_b1_1 = L.Linear(64, 512)
             # reshape from (1, 512) to (8, 8, 8)
             self.decoder_conv_b1_0 = L.Convolution2D(8, 8, ksize=3, pad=1) # (8, 8)
@@ -149,107 +155,104 @@ class Conv_Siam_VAE(chainer.Chain):
         return self.decode(encoded[0], encoded[1])
 
     def encode(self, x_b0, x_b1):
-        conv_b0_00_encoded = F.relu(self.encoder_conv_b0_00(x_b0)) # (200, 200)
+        conv_b0_00_encoded = F.leaky_relu(self.encoder_conv_b0_00(x_b0)) # (200, 200)
         pool_b0_00_encoded = F.max_pooling_2d(conv_b0_00_encoded, ksize=2) # (100, 100)
-        conv_b0_0_encoded = F.relu(self.encoder_conv_b0_0(pool_b0_00_encoded)) # (100, 100)
+        conv_b0_0_encoded = F.leaky_relu(self.encoder_conv_b0_0(pool_b0_00_encoded)) # (100, 100)
         pool_b0_0_encoded = F.max_pooling_2d(conv_b0_0_encoded, ksize=2) # (50, 50)
-        conv_b0_1_encoded = F.relu(self.encoder_conv_b0_1(pool_b0_0_encoded)) # (50, 50)
+        conv_b0_1_encoded = F.leaky_relu(self.encoder_conv_b0_1(pool_b0_0_encoded)) # (50, 50)
         pool_b0_1_encoded = F.max_pooling_2d(conv_b0_1_encoded, ksize=2) # (25, 25)
-        conv_b0_2_encoded = F.relu(self.encoder_conv_b0_2(pool_b0_1_encoded)) # (22, 22)
+        conv_b0_2_encoded = F.leaky_relu(self.encoder_conv_b0_2(pool_b0_1_encoded)) # (22, 22)
         pool_b0_2_encoded = F.max_pooling_2d(conv_b0_2_encoded, ksize=2) # (11, 11)
-        conv_b0_3_encoded = F.relu(self.encoder_conv_b0_3(pool_b0_2_encoded)) # (8, 8)
+        conv_b0_3_encoded = F.leaky_relu(self.encoder_conv_b0_3(pool_b0_2_encoded)) # (8, 8)
         reshaped_b0_encoded = F.reshape(conv_b0_3_encoded, (len(conv_b0_3_encoded), 1, 512)) # (1, 512)
-        dense_b0_0_encoded = self.encoder_dense_b0_0(reshaped_b0_encoded) # (1, 8)
+        dense_b0_0_encoded = F.leaky_relu(self.encoder_dense_b0_0(reshaped_b0_encoded)) # (1, 8)
         mu_b0 = self.encoder_mu_b0(dense_b0_0_encoded) # (1, 2)
         ln_var_b0 = self.encoder_ln_var_b0(dense_b0_0_encoded)  # (1, 2) log(sigma**2)
 
-        # conv_b1_00_encoded = F.relu(self.encoder_conv_b1_00(x_b1)) # (200, 200)
+        # conv_b1_00_encoded = F.leaky_relu(self.encoder_conv_b1_00(x_b1)) # (200, 200)
         # pool_b1_00_encoded = F.max_pooling_2d(conv_b1_00_encoded, ksize=2) # (100, 100)
-        # conv_b1_0_encoded = F.relu(self.encoder_conv_b1_0(pool_b1_00_encoded)) # (100, 100)
+        # conv_b1_0_encoded = F.leaky_relu(self.encoder_conv_b1_0(pool_b1_00_encoded)) # (100, 100)
         # pool_b1_0_encoded = F.max_pooling_2d(conv_b1_0_encoded, ksize=2) # (50, 50)
-        # conv_b1_1_encoded = F.relu(self.encoder_conv_b1_1(pool_b1_0_encoded)) # (50, 50)
+        # conv_b1_1_encoded = F.leaky_relu(self.encoder_conv_b1_1(pool_b1_0_encoded)) # (50, 50)
         # pool_b1_1_encoded = F.max_pooling_2d(conv_b1_1_encoded, ksize=2) # (25, 25)
-        # conv_b1_2_encoded = F.relu(self.encoder_conv_b1_2(pool_b1_1_encoded)) # (22, 22)
+        # conv_b1_2_encoded = F.leaky_relu(self.encoder_conv_b1_2(pool_b1_1_encoded)) # (22, 22)
         # pool_b1_2_encoded = F.max_pooling_2d(conv_b1_2_encoded, ksize=2) # (11, 11)
-        # conv_b1_3_encoded = F.relu(self.encoder_conv_b1_3(pool_b1_2_encoded)) # (8, 8)
+        # conv_b1_3_encoded = F.leaky_relu(self.encoder_conv_b1_3(pool_b1_2_encoded)) # (8, 8)
         # reshaped_b1_encoded = F.reshape(conv_b1_3_encoded, (len(conv_b1_3_encoded), 1, 512)) # (1, 512)
         # dense_b1_0_encoded = self.encoder_dense_b1_0(reshaped_b1_encoded) # (1, 8)
         # mu_b1 = self.encoder_mu_b1(dense_b1_0_encoded) # (1, 2)
         # ln_var_b1 = self.encoder_ln_var_b1(dense_b1_0_encoded)  # (1, 2) log(sigma**2)
 
-        conv_b1_00_encoded = F.relu(self.encoder_conv_b0_00(x_b1)) # (200, 200)
+        conv_b1_00_encoded = F.leaky_relu(self.encoder_conv_b0_00(x_b1)) # (200, 200)
         pool_b1_00_encoded = F.max_pooling_2d(conv_b1_00_encoded, ksize=2) # (100, 100)
-        conv_b1_0_encoded = F.relu(self.encoder_conv_b0_0(pool_b1_00_encoded)) # (100, 100)
+        conv_b1_0_encoded = F.leaky_relu(self.encoder_conv_b0_0(pool_b1_00_encoded)) # (100, 100)
         pool_b1_0_encoded = F.max_pooling_2d(conv_b1_0_encoded, ksize=2) # (50, 50)
-        conv_b1_1_encoded = F.relu(self.encoder_conv_b0_1(pool_b1_0_encoded)) # (50, 50)
+        conv_b1_1_encoded = F.leaky_relu(self.encoder_conv_b0_1(pool_b1_0_encoded)) # (50, 50)
         pool_b1_1_encoded = F.max_pooling_2d(conv_b1_1_encoded, ksize=2) # (25, 25)
-        conv_b1_2_encoded = F.relu(self.encoder_conv_b0_2(pool_b1_1_encoded)) # (22, 22)
+        conv_b1_2_encoded = F.leaky_relu(self.encoder_conv_b0_2(pool_b1_1_encoded)) # (22, 22)
         pool_b1_2_encoded = F.max_pooling_2d(conv_b1_2_encoded, ksize=2) # (11, 11)
-        conv_b1_3_encoded = F.relu(self.encoder_conv_b0_3(pool_b1_2_encoded)) # (8, 8)
+        conv_b1_3_encoded = F.leaky_relu(self.encoder_conv_b0_3(pool_b1_2_encoded)) # (8, 8)
         reshaped_b1_encoded = F.reshape(conv_b1_3_encoded, (len(conv_b1_3_encoded), 1, 512)) # (1, 512)
-        dense_b1_0_encoded = self.encoder_dense_b0_0(reshaped_b1_encoded) # (1, 8)
+        dense_b1_0_encoded = F.leaky_relu(self.encoder_dense_b0_0(reshaped_b1_encoded)) # (1, 8)
         mu_b1 = self.encoder_mu_b0(dense_b1_0_encoded) # (1, 2)
         ln_var_b1 = self.encoder_ln_var_b0(dense_b1_0_encoded)  # (1, 2) log(sigma**2)
-
-        # mu = F.add(mu_b0, -1 * mu_b1)
-        # ln_var = F.add(ln_var_b0, -1 * ln_var_b1)
-
-        # learn the operator over the two latent vectors (vector per object)
-        # concat_mu = F.concat((mu_b0, mu_b1), axis=1)
-        # concat_ln_var = F.concat((ln_var_b0, ln_var_b1), axis=1)
-
-        # mu_tmp = F.relu(self.operator_mu_0(concat_mu))
-        # ln_var_tmp = F.relu(self.operator_ln_var_0(concat_ln_var))
-
-        # mu = self.operator_mu_1(mu_tmp)
-        # ln_var = self.operator_ln_var_1(ln_var_tmp)
 
         return F.gaussian(mu_b0, ln_var_b0), F.gaussian(mu_b1, ln_var_b1), mu_b0, ln_var_b0, mu_b1, ln_var_b1
         
 
     def decode(self, z_b0, z_b1, sigmoid=True):
-        dense_b0_0_decoded = self.decoder_dense_b0_0(z_b0) # (1, 8)
-        dense_b0_1_decoded = self.decoder_dense_b0_1(dense_b0_0_decoded) # (1, 512)
+
+        _, latent, _ = self.predict_label(z_b0, z_b1)
+    	concat_b0 = F.concat((z_b1, latent), axis=1)	
+    	concat_b1 = F.concat((z_b0, latent), axis=1)
+
+        # dense_b0_0_decoded = self.decoder_dense_b0_0(z_b0) # (1, 8)
+        dense_b0_0_decoded = F.leaky_relu(self.decoder_dense_b0_0(concat_b0)) # (1, 10)
+        dense_b0_1_decoded = F.leaky_relu(self.decoder_dense_b0_1(dense_b0_0_decoded)) # (1, 512)
         reshapeb0_d_decoded = F.reshape(dense_b0_1_decoded, (len(dense_b0_1_decoded), 8, 8, 8))# (8, 8)
-        deconv_b0_0_decoded = F.relu(self.decoder_conv_b0_0(reshapeb0_d_decoded)) # (8, 8)
+        deconv_b0_0_decoded = F.leaky_relu(self.decoder_conv_b0_0(reshapeb0_d_decoded)) # (8, 8)
         up_b0_0_decoded = F.unpooling_2d(deconv_b0_0_decoded, ksize=2, cover_all=False) # (16, 16)
-        deconv_b0_1_decoded = F.relu(self.decoder_conv_b0_1(up_b0_0_decoded)) # (14, 14)
+        deconv_b0_1_decoded = F.leaky_relu(self.decoder_conv_b0_1(up_b0_0_decoded)) # (14, 14)
         up_b0_1_decoded = F.unpooling_2d(deconv_b0_1_decoded, ksize=2, cover_all=False) # (28, 28)
-        deconv_b0_2_decoded = F.relu(self.decoder_conv_b0_2(up_b0_1_decoded)) # (25, 25)
+        deconv_b0_2_decoded = F.leaky_relu(self.decoder_conv_b0_2(up_b0_1_decoded)) # (25, 25)
         up_b0_2_decoded = F.unpooling_2d(deconv_b0_2_decoded, ksize=2, cover_all=False) # (50, 50)
-        deconv_b0_3_decoded = F.relu(self.decoder_conv_b0_3(up_b0_2_decoded)) # (50, 50)
+        deconv_b0_3_decoded = F.leaky_relu(self.decoder_conv_b0_3(up_b0_2_decoded)) # (50, 50)
         up_b0_3_decoded = F.unpooling_2d(deconv_b0_3_decoded, ksize=2, cover_all=False) # (100, 100)
-        deconv_b0_4_decoded = F.relu(self.decoder_conv_b0_4(up_b0_3_decoded)) # (100, 100)
+        deconv_b0_4_decoded = F.leaky_relu(self.decoder_conv_b0_4(up_b0_3_decoded)) # (100, 100)
         up_b0_4_decoded = F.unpooling_2d(deconv_b0_4_decoded, ksize=2, cover_all=False) # (200, 200)
         out_img_b0 = self.decoder_output_img_b0(up_b0_4_decoded) # (200, 200)
 
+        # takes the encoding from the first branch and the spatial embedding as input
+        # to recreate the pointcloud of the second branch
         # dense_b1_0_decoded = self.decoder_dense_b1_0(z_b1) # (1, 8)
+        # dense_b1_0_decoded = self.decoder_dense_b1_0(concat_b1) # (1, 8)
         # dense_b1_1_decoded = self.decoder_dense_b1_1(dense_b1_0_decoded) # (1, 512)
         # reshapeb1_d_decoded = F.reshape(dense_b1_1_decoded, (len(dense_b1_1_decoded), 8, 8, 8))# (8, 8)
-        # deconv_b1_0_decoded = F.relu(self.decoder_conv_b1_0(reshapeb1_d_decoded)) # (8, 8)
+        # deconv_b1_0_decoded = F.leaky_relu(self.decoder_conv_b1_0(reshapeb1_d_decoded)) # (8, 8)
         # up_b1_0_decoded = F.unpooling_2d(deconv_b1_0_decoded, ksize=2, cover_all=False) # (16, 16)
-        # deconv_b1_1_decoded = F.relu(self.decoder_conv_b1_1(up_b1_0_decoded)) # (14, 14)
+        # deconv_b1_1_decoded = F.leaky_relu(self.decoder_conv_b1_1(up_b1_0_decoded)) # (14, 14)
         # up_b1_1_decoded = F.unpooling_2d(deconv_b1_1_decoded, ksize=2, cover_all=False) # (28, 28)
-        # deconv_b1_2_decoded = F.relu(self.decoder_conv_b1_2(up_b1_1_decoded)) # (25, 25)
+        # deconv_b1_2_decoded = F.leaky_relu(self.decoder_conv_b1_2(up_b1_1_decoded)) # (25, 25)
         # up_b1_2_decoded = F.unpooling_2d(deconv_b1_2_decoded, ksize=2, cover_all=False) # (50, 50)
-        # deconv_b1_3_decoded = F.relu(self.decoder_conv_b1_3(up_b1_2_decoded)) # (50, 50)
+        # deconv_b1_3_decoded = F.leaky_relu(self.decoder_conv_b1_3(up_b1_2_decoded)) # (50, 50)
         # up_b1_3_decoded = F.unpooling_2d(deconv_b1_3_decoded, ksize=2, cover_all=False) # (100, 100)
-        # deconv_b1_4_decoded = F.relu(self.decoder_conv_b1_4(up_b1_3_decoded)) # (100, 100)
+        # deconv_b1_4_decoded = F.leaky_relu(self.decoder_conv_b1_4(up_b1_3_decoded)) # (100, 100)
         # up_b1_4_decoded = F.unpooling_2d(deconv_b1_4_decoded, ksize=2, cover_all=False) # (200, 200)
         # out_img_b1 = self.decoder_output_img_b1(up_b1_4_decoded) # (200, 200)
 
-        dense_b1_0_decoded = self.decoder_dense_b0_0(z_b1) # (1, 8)
-        dense_b1_1_decoded = self.decoder_dense_b0_1(dense_b1_0_decoded) # (1, 512)
+        # dense_b1_0_decoded = F.leaky_relu(self.decoder_dense_b1_0(z_b1)) # (1, 8)
+        dense_b1_0_decoded = F.leaky_relu(self.decoder_dense_b1_0(concat_b1)) # (1, 10)
+        dense_b1_1_decoded = F.leaky_relu(self.decoder_dense_b1_1(dense_b1_0_decoded)) # (1, 512)
         reshapeb1_d_decoded = F.reshape(dense_b1_1_decoded, (len(dense_b1_1_decoded), 8, 8, 8))# (8, 8)
-        deconv_b1_0_decoded = F.relu(self.decoder_conv_b0_0(reshapeb1_d_decoded)) # (8, 8)
+        deconv_b1_0_decoded = F.leaky_relu(self.decoder_conv_b0_0(reshapeb1_d_decoded)) # (8, 8)
         up_b1_0_decoded = F.unpooling_2d(deconv_b1_0_decoded, ksize=2, cover_all=False) # (16, 16)
-        deconv_b1_1_decoded = F.relu(self.decoder_conv_b0_1(up_b1_0_decoded)) # (14, 14)
+        deconv_b1_1_decoded = F.leaky_relu(self.decoder_conv_b0_1(up_b1_0_decoded)) # (14, 14)
         up_b1_1_decoded = F.unpooling_2d(deconv_b1_1_decoded, ksize=2, cover_all=False) # (28, 28)
-        deconv_b1_2_decoded = F.relu(self.decoder_conv_b0_2(up_b1_1_decoded)) # (25, 25)
+        deconv_b1_2_decoded = F.leaky_relu(self.decoder_conv_b0_2(up_b1_1_decoded)) # (25, 25)
         up_b1_2_decoded = F.unpooling_2d(deconv_b1_2_decoded, ksize=2, cover_all=False) # (50, 50)
-        deconv_b1_3_decoded = F.relu(self.decoder_conv_b0_3(up_b1_2_decoded)) # (50, 50)
+        deconv_b1_3_decoded = F.leaky_relu(self.decoder_conv_b0_3(up_b1_2_decoded)) # (50, 50)
         up_b1_3_decoded = F.unpooling_2d(deconv_b1_3_decoded, ksize=2, cover_all=False) # (100, 100)
-        deconv_b1_4_decoded = F.relu(self.decoder_conv_b0_4(up_b1_3_decoded)) # (100, 100)
+        deconv_b1_4_decoded = F.leaky_relu(self.decoder_conv_b0_4(up_b1_3_decoded)) # (100, 100)
         up_b1_4_decoded = F.unpooling_2d(deconv_b1_4_decoded, ksize=2, cover_all=False) # (200, 200)
         out_img_b1 = self.decoder_output_img_b0(up_b1_4_decoded) # (200, 200)
 
@@ -261,44 +264,41 @@ class Conv_Siam_VAE(chainer.Chain):
     
     def predict_label(self, z_b0, z_b1, softmax=True):
         result = []
-        latent = []
 
         z_concat = F.concat((z_b0, z_b1), axis=1)
+        latent_pred = self.operators[0](z_concat)
+        latent = F.gaussian(latent_pred[:, :self.group_n], latent_pred[:, self.group_n:])
+        # latent = latent_pred[:, :2]
 
         for i in range(len(self.groups_len)):
-            # z = zs[:, i, None]#
-            z = self.operators[i](z_concat)
-            prediction = self.classifiers[i](z)
+            prediction = self.classifiers[i](latent[:, i, None])
 
-            latent.append(z.data)
             # need the check because the softmax_cross_entropy has a softmax in it
             if softmax:
                 result.append(F.softmax(prediction))
             else:
                 result.append(prediction)
 
-        return result, latent
-
-
-    # def get_latent(self, x_b0, x_b1):
-    #     mu, ln_var, _, _, _, _ = self.encode(x_b0, x_b1)
-    #     return F.gaussian(mu, ln_var)
-
-    # def get_latent_mu(self, x_b0, x_b1):
-    #     mu, ln_var, _, _, _, _ = self.encode(x_b0, x_b1)
-    #     return mu
+        return result, latent, latent_pred
 
 
     def get_latent(self, x_b0, x_b1):
         z_b0, z_b1, _, _, _, _ = self.encode(x_b0, x_b1)
-        _, latent = self.predict_label(z_b0, z_b1)
+        _, latent, _ = self.predict_label(z_b0, z_b1)
 
         return latent
 
 
+    def get_latent_pred(self, x_b0, x_b1):
+        z_b0, z_b1, _, _, _, _ = self.encode(x_b0, x_b1)
+        _, _, latent_pred = self.predict_label(z_b0, z_b1)
+
+        return latent_pred
+
+
     def get_label(self, x_b0, x_b1):
         z_b0, z_b1, _, _, _, _ = self.encode(x_b0, x_b1)
-        labels, _ = self.predict_label(z_b0, z_b1)
+        labels, _, _ = self.predict_label(z_b0, z_b1)
 
         return labels
 
@@ -342,7 +342,7 @@ class Conv_Siam_VAE(chainer.Chain):
                 # rec_loss += F.sum(rec_sum)
                 
 
-                out_labels, _ = self.predict_label(z_b0, z_b1, softmax=False)
+                out_labels, latent, latent_pred = self.predict_label(z_b0, z_b1, softmax=False)
                 for i in range(len(self.groups_len)):
                     n = self.groups_len[i] - 1
 
@@ -361,11 +361,23 @@ class Conv_Siam_VAE(chainer.Chain):
             self.label_loss = label_loss
             self.label_acc = label_acc
 
+            # latent_stacked = cupy.stack(latent, axis=1).reshape(batchsize, len(self.groups_len)).astype(cupy.float32)
+            # art_ln_var = cupy.zeros((batchsize, len(self.groups_len))).astype(cupy.float32)
+            
+            # print(latent_stacked)
+            # print(art_ln_var)
+            # print(latent_stacked.dtype)
+            # print(art_ln_var.dtype)
+            # print(latent_stacked.shape)
+            # print(art_ln_var.shape)
+            # print(mu_b1.shape)
+            # print(ln_var_b1.shape)
+
             kl = 0
-            # kl += 1 * gaussian_kl_divergence(mu[:,:2], ln_var[:,:2]) / (batchsize)
-            # kl += 1 * gaussian_kl_divergence(mu[:,2:], ln_var[:,2:]) / (batchsize)
-            kl += 1 * gaussian_kl_divergence(mu_b0, ln_var_b0) / (batchsize)
-            kl += 1 * gaussian_kl_divergence(mu_b1, ln_var_b1) / (batchsize)
+            kl += gaussian_kl_divergence(mu_b0, ln_var_b0) / (batchsize)
+            kl += gaussian_kl_divergence(mu_b1, ln_var_b1) / (batchsize)
+            # kl += gaussian_kl_divergence(latent_pred[:, :self.group_n], latent_pred[:, self.group_n:]) / (batchsize)
+
             self.kl = self.beta * kl
 
             # self.mu_difference = 100 * F.mean(F.squared_difference(mu[:,:2], cupy.tile(cupy.array([0,0], dtype=cupy.float32), (batchsize, 1))))
