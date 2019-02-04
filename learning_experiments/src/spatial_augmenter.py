bugfix offset calculation; bugfix directory path calculation

learning_experiments/src/spatial_augmenter.py
@@ -26,7 +26,7 @@ class SpatialAugmenter():
     def __init__(self, random_seed=0, verbose=0):
         self.verbose=verbose
 
-        # 2 cm
+        # 3 cm
         self.noise = 0.02
 
     def augment(self, branch_0, branch_1):
@@ -47,11 +47,14 @@ class SpatialAugmenter():
             ranges[0] = np.array(ranges[0])
 
             ranges.append(1 - np.max(points, axis=(0,1,2)))
+	    ranges[1][2] = 0
 
             for i in range(len(global_offsets)):
-                global_offsets[i] = np.random.uniform(low=ranges[0][i], high=ranges[1][i])
-                
-            # print(global_offsets)
+            	step_size = 7.5
+                step = (ranges[1][i] - ranges[0][i]) / step_size
+                global_offsets[i] = np.random.uniform(low= -step, high= step)
+
+            # print("global_offsets", global_offsets)
 
             # overall offset
             points += global_offsets
@@ -65,7 +68,7 @@ class SpatialAugmenter():
                 points[i] += local_offsets
                 points[i][points[i] == local_offsets] = 0
 
-#                 print(local_offsets)
+                # print("local_offsets", local_offsets)
                 
             for i in range(len(points)):
                 result[i].append(points[i])

learning_experiments/src/utils.py
@@ -9,6 +9,7 @@ python_version  :2.7.14
 """
 
 import os
+import os.path as osp
 import numpy as np
 from scipy.stats import multivariate_normal
 from scipy.stats import entropy
@@ -57,15 +58,19 @@ def clear_last_results(folder_name=None):
     all_files = list(filter(lambda filename : '.' in filename, os.listdir(folder_name)))
     map(lambda x : os.remove(folder_name + x), all_files)
 
-    leftover_folders = list(filter(lambda filename : filename != "models", os.listdir(folder_name)))
-    map(lambda x : shutil.rmtree(folder_name + x), leftover_folders)
+    # subfolders = ["scatter", "reconstruction_arrays"]
+    # for subfolder in subfolders:
+	   #  map(lambda x : os.remove(osp.join(folder_name, subfolder, x)), os.listdir(osp.join(folder_name, subfolder)))
 
-    os.mkdir(folder_name + "gifs")
-    os.mkdir(folder_name + "gifs/manifold_gif")
-    os.mkdir(folder_name + "gifs/scatter_gif")
-    os.mkdir(folder_name + "scatter")
-    os.mkdir(folder_name + "eval")
-    os.mkdir(folder_name + "reconstruction_arrays")
+    # leftover_folders = list(filter(lambda filename : filename != "models", os.listdir(folder_name)))
+    # map(lambda x : shutil.rmtree(folder_name + x), leftover_folders)
+
+    # os.mkdir(folder_name + "gifs")
+    # os.mkdir(folder_name + "gifs/manifold_gif")
+    # os.mkdir(folder_name + "gifs/scatter_gif")
+    # os.mkdir(folder_name + "scatter")
+    # os.mkdir(folder_name + "eval")
+    # os.mkdir(folder_name + "reconstruction_arrays")
 
 
 # for a given set of example images, calculate their reconstructions
@@ -121,7 +126,7 @@ def plot_loss_curves(stats=None, args=None):
     plt.xlabel("Epoch #", fontsize=14)
     plt.ylabel("Loss", fontsize=14)
     plt.legend(loc="upper left", bbox_to_anchor=(1, 1), fontsize=14)
-    plt.savefig(os.path.join(args.out + "losses"), bbox_inches="tight")
+    plt.savefig(os.path.join(args.out, "losses"), bbox_inches="tight")
     plt.close()
 
     # validation label loss
@@ -131,7 +136,7 @@ def plot_loss_curves(stats=None, args=None):
     plt.xlabel("Epoch #", fontsize=14)
     plt.ylabel("Loss", fontsize=14)
     plt.legend(loc="upper left", bbox_to_anchor=(1, 1), fontsize=14)
-    plt.savefig(os.path.join(args.out + "label_loss"), bbox_inches="tight")
+    plt.savefig(os.path.join(args.out, "label_loss"), bbox_inches="tight")
     plt.close()
 
     # validation label accuracy
@@ -141,7 +146,7 @@ def plot_loss_curves(stats=None, args=None):
     plt.xlabel("Epoch #", fontsize=14)
     plt.ylabel("Loss", fontsize=14)
     plt.legend(loc="upper left", bbox_to_anchor=(1, 1), fontsize=14)
-    plt.savefig(os.path.join(args.out + "valid_label_acc"), bbox_inches="tight")
+    plt.savefig(os.path.join(args.out, "valid_label_acc"), bbox_inches="tight")
     plt.close()
 
     # training label accuracy